835da937f322ec162e1db9c79e1cd1cc6ccfde74
dee123abc-123 <deepakpadmanabhan86@gmail.com>
2025-01-05 00:12:05 +0530

    Update pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,30 +1,21 @@
-def check_out() {
+def checkscm() {
     echo 'Checking out code...'
     checkout scm
 }
-
-def setup_java() {
+def setupjava() {
     echo 'Setting up Java 17...'
     sh 'sudo apt update'
     sh 'sudo apt install -y openjdk-17-jdk'
 }
-
-def setup_maven() {
+def mavensetup() {
     echo 'Setting up Maven...'
     sh 'sudo apt install -y maven'
 }
-
-def build_project() {
+def build() {
     echo 'Building project with Maven...'
     sh 'mvn clean package'
 }
-
-def upload_artifact(String artifactPath) {
-    echo 'Uploading artifact...'
-    archiveArtifacts artifacts: artifactPath, allowEmptyArchive: true
-}
-
-def run_application() {
+def runApp() {
     echo 'Running Spring Boot application...'
     sh 'nohup mvn spring-boot:run &'
     sleep(time: 15, unit: 'SECONDS')
@@ -32,8 +23,7 @@ def run_application() {
     def publicIp = sh(script: "curl -s https://checkip.amazonaws.com", returnStdout: true).trim()
     echo "The application is running and accessible at: http://${publicIp}:8080"
 }
-
-def validate_app() {
+def validateApp() {
     echo 'Validating that the app is running...'
     def response = sh(script: 'curl --write-out "%{http_code}" --silent --output /dev/null http://localhost:8080', returnStdout: true).trim()
     if (response == "200") {
@@ -43,11 +33,15 @@ def validate_app() {
         error("The app did not start correctly!")
     }
 }
-
-def stop_application() {
+def waiting() {
+        echo 'Waiting for 2 minutes...'
+        sleep(time: 2, unit: 'MINUTES')  // Wait for 2 minutes
+}
+def stop() {
     echo 'Gracefully stopping the Spring Boot application...'
     sh 'mvn spring-boot:stop'
 }
-
-
-
+def clean() {
+    echo 'Cleaning up...'
+    sh 'pkill -f "mvn spring-boot:run" || true'
+}
