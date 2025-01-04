cf7de037d9916e65368c47e8c14e6acf69e32966
dee123abc-123 <deepakpadmanabhan86@gmail.com>
2025-01-04 23:30:04 +0530

    Update pipeline.groovy

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -33,3 +33,21 @@ def run_application() {
     echo "The application is running and accessible at: http://${publicIp}:8080"
 }
 
+def validate_app() {
+    echo 'Validating that the app is running...'
+    def response = sh(script: 'curl --write-out "%{http_code}" --silent --output /dev/null http://localhost:8080', returnStdout: true).trim()
+    if (response == "200") {
+        echo 'The app is running successfully!'
+    } else {
+        echo "The app failed to start. HTTP response code: ${response}"
+        error("The app did not start correctly!")
+    }
+}
+
+def stop_application() {
+    echo 'Gracefully stopping the Spring Boot application...'
+    sh 'mvn spring-boot:stop'
+}
+
+
+
